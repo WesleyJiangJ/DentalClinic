7672f3dadd06fc6baf5b07bfe46a1040f21cb191
Wesley Jiang <wjiang917@gmail.com>
2024-03-28 13:34:37 -0600

    Delete "NewPatientModal" and "useDisclosure" imports

--- react_clinic/src/components/dashboard/Patient.jsx
+++ react_clinic/src/components/dashboard/Patient.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { useDisclosure } from "@nextui-org/react";
-import NewPatientModal from "./NewPatientModal";
 import PatientTable from "./PatientTable";
 
 export default function Patient() {
-    // const { isOpen, onOpen, onOpenChange } = useDisclosure();
     return (
         <>
-            {/* <PatientTable onOpen={onOpen} /> */}
             <PatientTable />
-            {/* <NewPatientModal isOpen={isOpen} onOpenChange={onOpenChange} /> */}
         </>
     );
 }
