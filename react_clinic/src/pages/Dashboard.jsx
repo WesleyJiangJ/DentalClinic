20769d7266f742e065622297e6faaaa663df8c6a
Wesley Jiang <wjiang917@gmail.com>
2024-03-28 10:19:07 -0600

    Add "patient_detail"

--- react_clinic/src/pages/Dashboard.jsx
+++ react_clinic/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Route, Routes } from 'react-router-dom';
 import Board from '../components/dashboard/Board.jsx';
 import Patient from '../components/dashboard/Patient.jsx';
+import PatientDetail from '../components/dashboard/PatientDetail.jsx'
 import Appointment from '../components/dashboard/Appointment.jsx';
 
 export default function Dashboard() {
@@ -34,10 +35,11 @@ export default function Dashboard() {
                         </div>
                     </div>
 
-                    <div className='m-4'>
+                    <div className='p-4'>
                         <Routes>
                             <Route path='' element={<Board />} />
                             <Route path='patient/*' element={<Patient />} />
+                            <Route path='patient/patient_detail' element={<PatientDetail />} />
                             <Route path='appointment/*' element={<Appointment />} />
                         </Routes>
                     </div>
