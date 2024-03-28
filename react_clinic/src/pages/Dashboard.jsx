f5da273028fad04c1d1d70306b1c786d908c822c
Wesley Jiang <wjiang917@gmail.com>
2024-03-28 14:19:42 -0600

    Remove "Routes" and add "Outlet"

--- react_clinic/src/pages/Dashboard.jsx
+++ react_clinic/src/pages/Dashboard.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import SideBar from '../components/dashboard/SideBar.jsx';
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { Route, Routes } from 'react-router-dom';
-import Board from '../components/dashboard/Board.jsx';
-import Patient from '../components/dashboard/Patient.jsx';
-import PatientDetail from '../components/dashboard/PatientDetail.jsx'
-import Appointment from '../components/dashboard/Appointment.jsx';
+import { Outlet } from 'react-router-dom';
 
 export default function Dashboard() {
     const [isToggled, setToggled] = React.useState(true);
@@ -17,7 +13,7 @@ export default function Dashboard() {
     return (
         <>
             <div className='flex'>
-                <SideBar collapsed={isToggled} onTitleChange={setTitle}/>
+                <SideBar collapsed={isToggled} onTitleChange={setTitle} />
                 <div className="flex flex-col w-full">
                     <div className='flex flex-row h-16 items-center bg-[rgb(249,249,249)]'>
                         <div className='mx-4 grow hidden md:flex'>
@@ -36,12 +32,7 @@ export default function Dashboard() {
                     </div>
 
                     <div className='p-4'>
-                        <Routes>
-                            <Route path='' element={<Board />} />
-                            <Route path='patient/*' element={<Patient />} />
-                            <Route path='patient/patient_detail' element={<PatientDetail />} />
-                            <Route path='appointment/*' element={<Appointment />} />
-                        </Routes>
+                        <Outlet />
                     </div>
                 </div>
             </div>
