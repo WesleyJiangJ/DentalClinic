d28c19611e50ab1b6c43b3a796e6956d44cc0663
Wesley Jiang <wjiang917@gmail.com>
2024-03-28 10:18:49 -0600

    Add the Link component to "Details"

--- react_clinic/src/components/dashboard/PatientTable.jsx
+++ react_clinic/src/components/dashboard/PatientTable.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 import { getAllPatients } from '../../api/patient_api.js'
 import NewPatientModal from "./NewPatientModal.jsx";
 import {
@@ -21,7 +22,7 @@ import {
     Tooltip,
     useDisclosure,
 } from "@nextui-org/react";
-import { PlusIcon, MagnifyingGlassIcon, ChevronDownIcon, ChevronDoubleLeftIcon, ChevronDoubleRightIcon, EyeIcon, PencilIcon, TrashIcon } from '@heroicons/react/24/solid'
+import { PlusIcon, MagnifyingGlassIcon, ChevronDownIcon, EyeIcon, PencilIcon, TrashIcon } from '@heroicons/react/24/solid'
 
 const statusColorMap = {
     true: "success",
@@ -162,17 +163,18 @@ export default function PatientTable() {
                 // );
                 return (
                     <div className="relative flex items-center gap-2">
-                        <Tooltip content="Details">
-                            <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                        <Tooltip content="Detalles">
+                            <Link to='patient_detail' className="text-lg text-default-400 cursor-pointer active:opacity-50">
                                 <EyeIcon className="w-5 h-5" />
-                            </span>
+                                <h1>{user.id}</h1>
+                            </Link>
                         </Tooltip>
-                        <Tooltip content="Edit user">
+                        <Tooltip content="Editar">
                             <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
                                 <PencilIcon className="w-5 h-5" />
                             </span>
                         </Tooltip>
-                        <Tooltip color="danger" content="Delete user">
+                        <Tooltip color="danger" content="Eliminar">
                             <span className="text-lg text-danger cursor-pointer active:opacity-50">
                                 <TrashIcon className="w-5 h-5" />
                             </span>
