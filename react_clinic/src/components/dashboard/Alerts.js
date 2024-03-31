219a92e9dcce2770d7a08753874b58e7636115e6
Wesley Jiang <wjiang917@gmail.com>
2024-03-31 17:20:47 -0600

    Change "position: "top-end""

--- react_clinic/src/components/dashboard/Alerts.js
+++ react_clinic/src/components/dashboard/Alerts.js
@@ -3,7 +3,7 @@ import Swal from 'sweetalert2'
 export function sweetToast(icon, title) {
     const Toast = Swal.mixin({
         toast: true,
-        position: "bottom-end",
+        position: "top-end",
         showConfirmButton: false,
         timer: 3000,
         timerProgressBar: true,
