6aa997ec17999b7656e387ddf4117a7fbb90a033
Wesley Jiang <wjiang917@gmail.com>
2024-04-01 17:32:31 -0600

    Add "Personal" routes

--- react_clinic/src/App.jsx
+++ react_clinic/src/App.jsx
@@ -4,8 +4,9 @@ import Clinic from './pages/Clinic.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import Board from './components/dashboard/Board.jsx';
 import Patient from './components/dashboard/Patient.jsx';
-import PatientDetail from './components/dashboard/PatientDetail.jsx';
+import Detail from './components/dashboard/Detail.jsx';
 import Appointment from './components/dashboard/Appointment.jsx';
+import Personal from './components/dashboard/personal/Personal.jsx';
 
 function App() {
   return (
@@ -15,8 +16,10 @@ function App() {
         <Route path='/dashboard/' element={<Dashboard />}>
           <Route path='' element={<Board />} />
           <Route path='patient/*' element={<Patient />} />
-          <Route path='patient/detail/:id' element={<PatientDetail />} />
+          <Route path='patient/detail/:id' element={<Detail value={"Paciente"} />} />
           <Route path='appointment/*' element={<Appointment />} />
+          <Route path='personal/*' element={<Personal />} />
+          <Route path='personal/detail/:id' element={<Detail value={"Personal"}/>} />
         </Route>
       </Routes>
     </>
