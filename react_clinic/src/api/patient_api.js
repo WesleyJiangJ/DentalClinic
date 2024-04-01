e5c1053a7646ef7d116388fdb38dbbecaf7f917b
Wesley Jiang <wjiang917@gmail.com>
2024-04-01 17:33:09 -0600

    Add POST, GET & PUT in "PersonalAPI"

--- react_clinic/src/api/patient_api.js
+++ react_clinic/src/api/patient_api.js
@@ -8,6 +8,11 @@ const addPatientAPI = axios.create({
     baseURL: 'http://localhost:8000/new_patient/'
 })
 
+const addPersonalAPI = axios.create({
+    baseURL: 'http://localhost:8000/personal/'
+})
+
+// Patient
 export const getAllPatients = () => {
     return patientAPI.get('/')
 }
@@ -20,6 +25,23 @@ export const updatePatient = (id, data) => {
     return patientAPI.put(`/${id}/`, data)
 }
 
-export const postNewPatient = (patient) => {
-    return addPatientAPI.post('/', patient)
+export const postNewPatient = (data) => {
+    return addPatientAPI.post('/', data)
+}
+
+// Personal
+export const getAllPersonal = () => {
+    return addPersonalAPI.get('/')
+}
+
+export const getSpecificPersonal = (id) => {
+    return addPersonalAPI.get(`/${id}`)
+}
+
+export const updatePersonal = (id, data) => {
+    return addPersonalAPI.put(`/${id}/`, data)
+}
+
+export const postNewPersonal = (data) => {
+    return addPersonalAPI.post('/', data)
 }
