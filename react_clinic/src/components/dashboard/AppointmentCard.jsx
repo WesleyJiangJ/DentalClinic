38bd370a36274c521c4934f60d2715a1c3cc0d58
Wesley Jiang <wjiang917@gmail.com>
2024-04-05 14:17:42 -0600

    Parameters were added to main function

--- react_clinic/src/components/dashboard/AppointmentCard.jsx
+++ react_clinic/src/components/dashboard/AppointmentCard.jsx
@@ -1,32 +1,32 @@
 import { Button, Card, CardHeader, CardBody, CardFooter } from '@nextui-org/react'
 import { PencilIcon, CheckIcon } from "@heroicons/react/24/solid";
 
-export default function AppointmentCard() {
+export default function AppointmentCard({ observation, patient, personal, date }) {
     return (
         <>
             <div className='m-2'>
                 <Card shadow='none' className='bg-[#1E1E1E] text-white' radius='sm'>
                     <CardHeader>
-                        <h1>Patient Name</h1>
+                        <h1 className='text-xl font-bold'>{observation}</h1>
                     </CardHeader>
                     <CardBody>
-                        <p>Date and Doctor's Name</p>
+                        <p>{patient}</p>
+                        <p>{personal}</p>
+                        <p>{date}</p>
                     </CardBody>
                     <CardFooter>
                         <div className='flex flex-row w-full gap-1'>
                             <div className='w-full'>
                                 <Button
                                     className='w-full bg-white'
-                                    radius='sm'
-                                >
+                                    radius='sm'>
                                     <PencilIcon className="h-5 w-5" />
                                 </Button>
                             </div>
                             <div className='w-full'>
                                 <Button
                                     className='w-full bg-white'
-                                    radius='sm'
-                                >
+                                    radius='sm'>
                                     <CheckIcon className="h-5 w-5" />
                                 </Button>
                             </div>
