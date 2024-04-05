4cd0f64220fd913b9e8aa948e96769d2b5973660
Wesley Jiang <wjiang917@gmail.com>
2024-04-05 11:28:40 -0600

    handleInputChange was added

--- react_clinic/src/components/dashboard/NewAppointmentModal.jsx
+++ react_clinic/src/components/dashboard/NewAppointmentModal.jsx
@@ -1,15 +1,44 @@
 import React from "react";
+import { getAllPatients, getAllPersonal } from "../../api/apiFunctions";
 import { Input, Select, SelectItem, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Textarea } from "@nextui-org/react";
 
 export default function NewAppointmentModal({ isOpen, onOpenChange }) {
-    const patients = [
-        { label: "Génesis Rashel Téllez Vargas", value: "1" },
-        { label: "Wesley Wen Jiang Jacamo", value: "2" },
-    ]
-    const doctors = [
-        { label: "Lucia del Socorro Rodriguez Vargas", value: "1" },
-        { label: "Wendy Walkiria Jácamo Rodriguez", value: "2" },
-    ]
+    const [patientData, setPatientData] = React.useState([]);
+    const [id_patient, setIdPatient] = React.useState('');
+    const [personalData, setPersonalData] = React.useState([]);
+    const [id_personal, setIdPersonal] = React.useState('');
+    const [dateValue, setDateValue] = React.useState(new Date());
+    const [timeValue, setTimeValue] = React.useState('');
+    const [observations, setObservations] = React.useState('');
+
+    const loadData = async () => {
+        const patientRes = await getAllPatients();
+        const personalRes = await getAllPersonal();
+        setPatientData(patientRes.data);
+        setPersonalData(personalRes.data);
+    }
+
+    React.useEffect(() => {
+        loadData();
+    }, []);
+
+    const handleInputChange = (e, setInputFunction) => {
+        const input = e.target.value;
+        setInputFunction(input);
+    }
+
+    const formData = {
+        id_patient,
+        id_personal,
+        datetime: dateValue + ' ' + timeValue,
+        observations,
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formData)
+    }
+
     return (
         <>
             <Modal
@@ -22,59 +51,71 @@ export default function NewAppointmentModal({ isOpen, onOpenChange }) {
                 <ModalContent>
                     {(onClose) => (
                         <>
-                            <ModalHeader className="flex flex-col gap-1">Nueva Cita</ModalHeader>
-                            <ModalBody>
-                                <div className="flex flex-col gap-5">
-                                    <div className="flex flex-col md:flex-row gap-4">
-                                        <Select
-                                            label="Paciente"
-                                            variant="underlined">
-                                            {patients.map((patient) => (
-                                                <SelectItem key={patient.value} value={patient.value}>
-                                                    {patient.label}
-                                                </SelectItem>
-                                            ))}
-                                        </Select>
-                                        <Select
-                                            label="Doctor"
-                                            variant="underlined">
-                                            {doctors.map((doctor) => (
-                                                <SelectItem key={doctor.value} value={doctor.value}>
-                                                    {doctor.label}
-                                                </SelectItem>
-                                            ))}
-                                        </Select>
-                                    </div>
-                                    <div className="flex flex-col md:flex-row gap-4">
-                                        <Input
-                                            label="Fecha"
-                                            placeholder="a"
-                                            type="date"
-                                            variant="underlined"
+                            <form onSubmit={handleSubmit}>
+                                <ModalHeader className="flex flex-col gap-1">Nueva Cita</ModalHeader>
+                                <ModalBody>
+                                    <div className="flex flex-col gap-5">
+                                        <div className="flex flex-col md:flex-row gap-4">
+                                            <Select
+                                                label="Paciente"
+                                                variant="underlined"
+                                                value={id_patient}
+                                                onChange={(e) => handleInputChange(e, setIdPatient)}>
+                                                {patientData.map((patient) => (
+                                                    <SelectItem key={patient.id} value={patient.id} textValue={patient.first_name + ' ' + patient.middle_name + ' ' + patient.first_lastname + ' ' + patient.second_lastname}>
+                                                        {patient.first_name} {patient.middle_name} {patient.first_lastname} {patient.second_lastname}
+                                                    </SelectItem>
+                                                ))}
+                                            </Select>
+                                            <Select
+                                                label="Doctor"
+                                                variant="underlined"
+                                                value={id_personal}
+                                                onChange={(e) => handleInputChange(e, setIdPersonal)}>
+                                                {personalData.map((personal) => (
+                                                    <SelectItem key={personal.id} value={personal.id} textValue={personal.first_name + ' ' + personal.middle_name + ' ' + personal.first_lastname + ' ' + personal.second_lastname}>
+                                                        {personal.first_name} {personal.middle_name} {personal.first_lastname} {personal.second_lastname}
+                                                    </SelectItem>
+                                                ))}
+                                            </Select>
+                                        </div>
+                                        <div className="flex flex-col md:flex-row gap-4">
+                                            <Input
+                                                label="Fecha"
+                                                placeholder="a"
+                                                type="date"
+                                                variant="underlined"
+                                                value={dateValue}
+                                                onChange={(e) => handleInputChange(e, setDateValue)}
+                                            />
+                                            <Input
+                                                label="Hora"
+                                                placeholder="a"
+                                                type="time"
+                                                variant="underlined"
+                                                value={timeValue}
+                                                onChange={(e) => handleInputChange(e, setTimeValue)}
+                                            />
+                                        </div>
+                                        <Textarea
+                                            label="Observaciones"
+                                            placeholder="Escriba aquí . . ."
+                                            size="lg"
+                                            radius="sm"
+                                            value={observations}
+                                            onChange={(e) => handleInputChange(e, setObservations)}
                                         />
-                                        <Input
-                                            label="Hora"
-                                            placeholder="a"
-                                            type="time"
-                                            variant="underlined"
-                                        />
                                     </div>
-                                    <Textarea
-                                        label="Observaciones"
-                                        placeholder="Escriba aquí . . ."
-                                        size="lg"
-                                        radius="sm"
-                                    />
-                                </div>
-                            </ModalBody>
-                            <ModalFooter>
-                                <Button color="danger" variant="light" onPress={onClose}>
-                                    Cerrar
-                                </Button>
-                                <Button color="primary" onPress={onClose}>
-                                    Agendar
-                                </Button>
-                            </ModalFooter>
+                                </ModalBody>
+                                <ModalFooter>
+                                    <Button color="danger" variant="light" onPress={onClose}>
+                                        Cerrar
+                                    </Button>
+                                    <Button color="primary" type="submit">
+                                        Agendar
+                                    </Button>
+                                </ModalFooter>
+                            </form>
                         </>
                     )}
                 </ModalContent>
