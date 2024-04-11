45d85bafea0af9024758c15161d38a289cbb9126
Wesley Jiang <wjiang917@gmail.com>
2024-04-11 14:38:48 -0600

    appointments was added

--- api/urls.py
+++ api/urls.py
@@ -5,19 +5,21 @@ from rest_framework.documentation import include_docs_urls
 from api import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'patient', views.PatientViewSet)
-router.register(r'personal', views.PersonalViewSet)
-router.register(r'new_patient', views.NewPatientViewSet)
-router.register(r'appointment', views.AppointmentViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+router.register(r"patient", views.PatientViewSet)
+router.register(r"personal", views.PersonalViewSet)
+router.register(r"new_patient", views.NewPatientViewSet)
+router.register(r"appointment", views.AppointmentViewSet)
+router.register(r"appointments", views.Appointments, basename="appointments")
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('docs/', include_docs_urls(title= "API"))
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("docs/", include_docs_urls(title="API")),
 ]
 
 urlpatterns += router.urls
