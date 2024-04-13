4c1c636d6ba6f598dfc1714f7915931da8a17283
Wesley Jiang <wjiang917@gmail.com>
2024-04-13 11:10:40 -0600

    Check if the appointment has expired

--- react_clinic/src/components/dashboard/AppointmentCard.jsx
+++ react_clinic/src/components/dashboard/AppointmentCard.jsx
@@ -2,13 +2,38 @@ import React from 'react';
 import { Button, Card, CardHeader, CardBody, CardFooter } from '@nextui-org/react'
 import { PencilIcon, CheckIcon, CalendarDaysIcon, ClockIcon } from "@heroicons/react/24/solid";
 
-export default function AppointmentCard({ id, reason, patient, personal, date, onOpen, navigate, view, observation }) {
+export default function AppointmentCard({ id, reason, patient, personal, date, onOpen, navigate, view, observation, appointmentType }) {
     const formattedDate = new Date(date).toISOString().split('T')[0];
     const formattedTime = new Date(date).toISOString().split('T')[1].slice(0, 5);
+
+    // Get actual date and yesterday date
+    const currentDate = new Date();
+    const yesterdayDate = new Date(currentDate);
+    yesterdayDate.setDate(currentDate.getDate() - 1);
+
+    // Convert appointment date to an object Date
+    const appointmentDate = new Date(date);
+
+    // Check if the appointment date is earlier than yesterday
+    const isPastAppointment = appointmentDate < yesterdayDate;
+
     return (
         <>
             <div className='m-2'>
-                <Card shadow='none' className='bg-[#1E1E1E] text-white' radius='sm'>
+                <Card
+                    shadow='none'
+                    className={
+                        isPastAppointment === false
+                            ?
+                            'bg-[#1E1E1E] text-white'
+                            :
+                            appointmentType === 'done' || appointmentType === 'cancelled'
+                                ?
+                                'bg-[#1E1E1E] text-white'
+                                :
+                                'bg-[#B63E3E] text-white'
+                    }
+                    radius='sm'>
                     <CardHeader>
                         <div className="flex flex-col">
                             <h1 className='text-xl font-bold'>{reason}</h1>
@@ -41,17 +66,19 @@ export default function AppointmentCard({ id, reason, patient, personal, date, o
                                         <PencilIcon className="h-5 w-5" />
                                     </Button>
                                 </div>
-                                <div className='w-full'>
-                                    <Button
-                                        className='w-full bg-white'
-                                        radius='sm'
-                                        onPress={() => {
-                                            onOpen();
-                                            navigate(`check/${id}`);
-                                        }}>
-                                        <CheckIcon className="h-5 w-5" />
-                                    </Button>
-                                </div>
+                                {isPastAppointment !== true &&
+                                    <div className='w-full'>
+                                        <Button
+                                            className='w-full bg-white'
+                                            radius='sm'
+                                            onPress={() => {
+                                                onOpen();
+                                                navigate(`check/${id}`);
+                                            }}>
+                                            <CheckIcon className="h-5 w-5" />
+                                        </Button>
+                                    </div>
+                                }
                             </div>
                         </CardFooter >
                     }
