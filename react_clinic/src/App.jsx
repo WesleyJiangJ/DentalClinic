9524eca0197f6309787e45f108b35765b8cd4a14
Wesley Jiang <wjiang917@gmail.com>
2024-05-07 11:22:17 -0600

    Create and add "DashboardContext"

--- react_clinic/src/App.jsx
+++ react_clinic/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import DashboardContext from './components/dashboard/DashboardContext.js';
 import Clinic from './pages/Clinic.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import Board from './components/dashboard/Board.jsx';
@@ -12,24 +13,36 @@ import Settings from './components/dashboard/Settings.jsx';
 import TreatmentModal from './components/dashboard/TreatmentModal.jsx';
 
 function App() {
+  const dashboardData = {
+    titles: {
+      dashboard: 'Dashboard',
+      patient: 'Pacientes',
+      appointment: "Citas",
+      payments: "Pagos",
+      personal: "Personal",
+      settings: "Ajustes",
+    },
+  };
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Clinic />} />
-        <Route path='/dashboard/' element={<Dashboard />}>
-          <Route path='' element={<Board />} />
-          <Route path='patient/*' element={<Patient />} />
-          <Route path='patient/detail/:id' element={<Detail value={"Paciente"} />} />
-          <Route path='appointment/*' element={<Appointment />} />
-          <Route path='appointment/:slug/:id' element={<Appointment />} />
-          <Route path='payments/*' element={<Payments />} />
-          <Route path='personal/*' element={<Personal />} />
-          <Route path='personal/detail/:id' element={<Detail value={"Personal"} />} />
-          <Route path='settings/*' element={<Settings />}>
-            <Route path='treatment/:id' element={<TreatmentModal />} />
+      <DashboardContext.Provider value={dashboardData}>
+        <Routes>
+          <Route path='/' element={<Clinic />} />
+          <Route path='/dashboard/' element={<Dashboard />}>
+            <Route path='' element={<Board />} />
+            <Route path='patient/*' element={<Patient />} />
+            <Route path='patient/detail/:id' element={<Detail value={"Paciente"} />} />
+            <Route path='appointment/*' element={<Appointment />} />
+            <Route path='appointment/:slug/:id' element={<Appointment />} />
+            <Route path='payments/*' element={<Payments />} />
+            <Route path='personal/*' element={<Personal />} />
+            <Route path='personal/detail/:id' element={<Detail value={"Personal"} />} />
+            <Route path='settings/*' element={<Settings />}>
+              <Route path='treatment/:id' element={<TreatmentModal />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </DashboardContext.Provider>
     </>
   );
 }
