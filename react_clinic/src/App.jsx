65aa5d704fc793a163ea7e40aa2e27aab6010323
Wesley Jiang <wjiang917@gmail.com>
2024-05-11 15:16:39 -0600

    PaymentMOdal was added

--- react_clinic/src/App.jsx
+++ react_clinic/src/App.jsx
@@ -8,6 +8,7 @@ import Patient from './components/dashboard/Patient.jsx';
 import Detail from './components/dashboard/Detail.jsx';
 import Appointment from './components/dashboard/Appointment.jsx';
 import Payments from './components/dashboard/Payments.jsx';
+import PaymentModal from './components/dashboard/PaymentModal.jsx';
 import Personal from './components/dashboard/Personal.jsx';
 import Settings from './components/dashboard/Settings.jsx';
 import TreatmentModal from './components/dashboard/TreatmentModal.jsx';
@@ -34,7 +35,9 @@ function App() {
             <Route path='patient/detail/:id' element={<Detail value={"Paciente"} />} />
             <Route path='appointment/*' element={<Appointment />} />
             <Route path='appointment/:slug/:id' element={<Appointment />} />
-            <Route path='payments/*' element={<Payments />} />
+            <Route path='payments/*' element={<Payments />}>
+              <Route path='detail/:id' element={<PaymentModal />} />
+            </Route>
             <Route path='personal/*' element={<Personal />} />
             <Route path='personal/detail/:id' element={<Detail value={"Personal"} />} />
             <Route path='settings/*' element={<Settings />}>
