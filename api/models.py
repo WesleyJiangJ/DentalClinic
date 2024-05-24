6c01b0ed0e8d040796c0fb8bdf1b1d9806f67c24
Wesley Jiang <wjiang917@gmail.com>
2024-05-24 14:54:30 -0600

    Added "total" and "type" fields to Budget modal and created "PaymentControl" model

--- api/models.py
+++ api/models.py
@@ -245,6 +245,8 @@ class Budget(models.Model):
     id_patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=256)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=1)
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -263,12 +265,11 @@ class Budget_Detail(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-# class Payment_Control(models.Model):
-#     id_budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
-#     total = models.DecimalField(max_digits=10, decimal_places=2)
-#     paid = models.DecimalField(max_digits=10, decimal_places=2)
-#     description = models.TextField(null=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
+class PaymentControl(models.Model):
+    id_budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    paid = models.DecimalField(max_digits=10, decimal_places=2)
+    note = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 # class Payment_Control_History(models.Model):
