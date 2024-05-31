1df68e0ef3f8a626d363c7687a8369976c3cab9f
Wesley Jiang <wjiang917@gmail.com>
2024-05-30 18:13:43 -0600

    Create BudPayCard component - It adds the BudPayCard component, which displays some Budget and Payment information within the patient's details.

---
+++ react_clinic/src/components/dashboard/BudPayCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, CardHeader, CardBody } from '@nextui-org/react'
+
+export default function BudPayCard({ name, description, total, treatmentQuantity }) {
+    return (
+        <>
+            <div className='m-2'>
+                <Card
+                    shadow='none'
+                    radius='sm'
+                    className='bg-primary text-white'>
+                    <CardHeader>
+                        <div className="flex flex-col">
+                            <h1 className='text-xl font-bold'>{name}</h1>
+                            <p className='text-md font-thin'>{description}</p>
+                        </div>
+                    </CardHeader>
+                    <CardBody>
+                        <p>{total}</p>
+                        <p>{treatmentQuantity}</p>
+                    </CardBody>
+                </Card >
+            </div >
+        </>
+    );
+}
