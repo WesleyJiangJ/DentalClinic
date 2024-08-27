165d4c6ba564dfd194050c146c2f0ff63b5a70b2
Wesley Jiang <wjiang917@gmail.com>
2024-08-27 17:51:54 -0600

    Update FindUs component with embedded Google Map and updated address
    - Replaced Google Maps API component with an embedded iframe for simpler integration.
    - Updated header text to "¿Dónde puedes encontrarnos?".
    - Adjusted map container height to 50rem for better visibility.
    - Retained address details with slight formatting for clarity.

--- frontend/src/components/clinic/FindUs.jsx
+++ frontend/src/components/clinic/FindUs.jsx
@@ -1,25 +1,16 @@
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
-import { useMemo } from "react";
-
 export default function FindUs() {
-    // const { isLoaded } = useLoadScript({
-    //     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-    // });
-    const center = useMemo(() => ({ lat: 12.136389, lng: -86.251389 }), []);
     return (
-        <>
-            <h1 className='text-2xl lg:text-6xl font-semibold text-center py-10'>¿Dónde encontrarnos?</h1>
+        <div className="h-1/2">
+            <h1 className='text-2xl md:text-6xl font-semibold text-center py-10'>¿Dónde puedes encontrarnos?</h1>
             <div className="grid grid-cols-1 lg:grid-cols-2">
-                <div>
-                    {/* {!isLoaded ? (
-                        <h1>Loading...</h1>
-                    ) : (
-                        <GoogleMap
-                            mapContainerClassName="w-full h-[650px] rounded-lg"
-                            center={center}
-                            zoom={10}
-                        />
-                    )} */}
+                <div className="h-[50rem]">
+                    <iframe
+                        className="w-full h-full rounded-md"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19748.861899799846!2d-86.18673272921484!3d12.139179228051324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f73fbfc3f69cac7%3A0x23fd903b3fad5c5a!2sAeropuerto%20Internacional%20Augusto%20C.%20Sandino!5e0!3m2!1ses-419!2sni!4v1724800016566!5m2!1ses-419!2sni"
+                        allow="fullscreen"
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    />
                 </div>
                 <div className="grid place-content-center">
                     <p className="text-center p-5 text-md lg:text-2xl">
@@ -31,6 +22,6 @@ export default function FindUs() {
                     </p>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
