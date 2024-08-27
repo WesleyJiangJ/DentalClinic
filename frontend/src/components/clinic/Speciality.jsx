78c1edd621173beda6589b5c36c9e8f698a31525
Wesley Jiang <wjiang917@gmail.com>
2024-08-27 17:49:57 -0600

    Update Speciality component with static images and descriptions
    - Replaced online image URLs with local static images for each specialty.
    - Added descriptions for each specialty to provide more context and information to users.
    - Adjusted Carousel settings for smoother transitions.
    - Refactored layout for better alignment and spacing in the Speciality component.

--- frontend/src/components/clinic/Speciality.jsx
+++ frontend/src/components/clinic/Speciality.jsx
@@ -4,67 +4,60 @@ export default function Speciality() {
     const specialties = [
         {
             text: 'Odontología General',
-            image: 'https://images.unsplash.com/photo-1606811841689-23dfddce3e95?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            alt: 'Odontología General Image'
-
+            image: '../images/GeneralDentistry.jpeg',
+            alt: 'GeneralDentistry Image',
+            description: 'La odontología general se encarga del cuidado integral de tu salud bucal, desde la prevención hasta el tratamiento de enfermedades comunes como caries y gingivitis.'
         },
         {
             text: 'Odontopediatría',
-            image: 'https://images.unsplash.com/photo-1606811971618-4486d14f3f99?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            alt: 'Odontopediatría Image'
+            image: '../images/Odontopediatria.jpeg',
+            alt: 'Odontopediatría Image',
+            description: 'Especializada en el cuidado dental de los más pequeños, la odontopediatría ofrece tratamientos adaptados a las necesidades de los niños, asegurando su bienestar desde temprana edad.'
         },
         {
             text: 'Ortodoncia',
-            image: 'https://images.unsplash.com/photo-1609840114035-3c981b782dfe?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            alt: 'Ortodoncia Image'
+            image: '../images/Orthodontics.jpeg',
+            alt: 'Orthodontics Image',
+            description: 'La ortodoncia se enfoca en corregir problemas de alineación y mordida, utilizando aparatos que te ayudarán a lograr una sonrisa recta y saludable.'
         },
         {
             text: 'Cirugía Oral y Maxilofacial',
-            image: 'https://images.unsplash.com/photo-1588776814546-1ffcf47267a5?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            alt: 'Cirugía Oral y Maxilofacial Image'
+            image: '../images/OralMaxillofacialSurgery.jpeg',
+            alt: 'Oral and Maxillofacial Surgery Image',
+            description: 'Esta especialidad trata problemas más complejos que afectan la boca, mandíbula y cara, realizando desde extracciones de muelas del juicio hasta cirugías reconstructivas.'
         },
-        {
-            text: 'Test',
-            image: 'https://images.unsplash.com/photo-1682686578615-39549501dd08?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            alt: 'Test I'
-        },
-    ]
+    ];
 
     return (
-        <>
-            <h1 className='text-2xl lg:text-6xl font-semibold text-center py-10'>Especialidades</h1>
-            <Carousel autoplay loop className="rounded-xl w-full h-[450px] lg:h-[750px] mt-3">
-                {specialties.map((s, index) => (
+        <div className="flex flex-col gap-2 h-screen">
+            <h1 className='text-2xl md:text-6xl font-semibold text-center my-10'>Especialidades</h1>
+            <Carousel autoplay loop transition={{ duration: 2 }} className="rounded-xl w-full">
+                {specialties.map((item, index) => (
                     <div key={index} className="relative h-full w-full">
                         <img
-                            src={s.image}
-                            alt={s.alt}
+                            src={item.image}
+                            alt={item.alt}
                             className="h-full w-full object-cover"
                         />
-                        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/50">
+                        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/30">
                             <div className="w-3/4 text-center md:w-2/4">
                                 <Typography
                                     variant="h1"
                                     color="white"
-                                    className="mb-4 text-2xl md:text-4xl lg:text-5xl"
-                                >
-                                    {s.text}
+                                    className="mb-4 text-2xl md:text-4xl lg:text-5xl">
+                                    {item.text}
                                 </Typography>
                                 <Typography
                                     variant="lead"
                                     color="white"
-                                    className="mb-12 opacity-80 text-md lg:text-xl"
-                                >
-                                    It is not so much for its beauty that the forest makes a claim
-                                    upon men&apos;s hearts, as for that subtle something, that quality
-                                    of air that emanation from old trees, that so wonderfully changes
-                                    and renews a weary spirit.
+                                    className="mb-12 opacity-80 text-md lg:text-xl">
+                                    {item.description}
                                 </Typography>
                             </div>
                         </div>
                     </div>
                 ))}
             </Carousel>
-        </>
+        </div>
     );
 }
