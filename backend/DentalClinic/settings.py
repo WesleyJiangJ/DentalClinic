0a4e39fa6118f0aa48b8454008d69547eed916ef
Wesley Jiang <wjiang917@gmail.com>
2024-08-28 15:25:36 -0600

    Configure email backend for SMTP
    - Added email backend configuration to send emails using SMTP.
    - Configured `EMAIL_BACKEND`, `EMAIL_HOST`, `EMAIL_PORT`, `EMAIL_USE_TLS`, `EMAIL_HOST_USER`, and `EMAIL_HOST_PASSWORD`.
    - Environment variables `EMAIL_HOST_USER` and `EMAIL_HOST_PASSWORD` are used to store sensitive information.

--- backend/DentalClinic/settings.py
+++ backend/DentalClinic/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,3 +139,11 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = "api.User"
+
+# Send Email
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
