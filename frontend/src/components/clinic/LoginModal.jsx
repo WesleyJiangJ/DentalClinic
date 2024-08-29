970390b98c8f2bd7c5eac9905c7a16d4c10d0c36
Wesley Jiang <wjiang917@gmail.com>
2024-08-29 17:03:25 -0600

    Add form validation and API integration
    - Integrated `react-hook-form` for managing form state and validation.
    - Implemented user authentication with API call to retrieve access and refresh tokens.
    - Enhanced user experience by adding password visibility toggle.
    - Improved error handling for login attempts, displaying specific error messages.
    - Reset form fields upon closing the modal or after a failed login attempt.

--- frontend/src/components/clinic/LoginModal.jsx
+++ frontend/src/components/clinic/LoginModal.jsx
@@ -1,10 +1,39 @@
+import React from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useForm, Controller } from 'react-hook-form';
+import { getUser } from '../../api/apiFunctions.js'
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Input } from "@nextui-org/react";
+import { UserIcon, LockClosedIcon, EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline"
 
 export default function LoginModal({ isOpen, onOpenChange }) {
     const navigate = useNavigate();
-    const handleClick = () => {
-        navigate('/dashboard');
+    const [isVisiblePassword, setIsVisiblePassword] = React.useState(false);
+    const toggleVisibility = () => setIsVisiblePassword(!isVisiblePassword);
+    const { control, handleSubmit, formState: { errors }, reset, setError } = useForm({
+        defaultValues: {
+            username: '',
+            password: ''
+        }
+    });
+
+    const onSubmit = async (data) => {
+        try {
+            const response = await axios.post('http://localhost:8000/api/token/', {
+                username: data.username,
+                password: data.password,
+            });
+            localStorage.setItem('access_token', response.data.access);
+            localStorage.setItem('refresh_token', response.data.refresh);
+            navigate('/dashboard');
+            const name = (await getUser(data.username)).data;
+            localStorage.setItem('name', `${name[0].first_name} ${name[0].last_name}`);
+            reset();
+        } catch (error) {
+            reset();
+            setError('username');
+            setError('password', { message: error.response.data.detail });
+        }
     };
 
     return (
@@ -12,62 +41,96 @@ export default function LoginModal({ isOpen, onOpenChange }) {
             <Modal
                 backdrop="blur"
                 isOpen={isOpen}
-                onOpenChange={onOpenChange}
+                onOpenChange={() => {
+                    onOpenChange(true);
+                    reset();
+                }}
                 placement="top-center"
                 size="2xl"
                 radius="sm">
                 <ModalContent>
                     {(onClose) => (
                         <>
-                            <div className="grid grid-cols-1 lg:grid-cols-2">
-                                <div>
-                                    <ModalHeader className="flex flex-col gap-1">Bienvenido</ModalHeader>
-                                    <ModalBody>
-                                        <Input
-                                            autoFocus
-                                            label="Correo"
-                                            type="email"
-                                            placeholder="Ingresa tu correo"
-                                            variant="bordered"
-                                            radius="sm"
-                                        />
-                                        <Input
-                                            label="Contraseña"
-                                            placeholder="Ingresa tu contraseña"
-                                            type="password"
-                                            variant="bordered"
-                                            radius="sm"
-                                        />
-                                    </ModalBody>
-                                    <ModalFooter>
-                                        <Button
-                                            color="danger"
-                                            variant="flat"
-                                            radius="sm"
-                                            onPress={onClose}>
-                                            Cerrar
-                                        </Button>
-                                        <Button
-                                            color='primary'
-                                            radius="sm"
-                                            onPress={handleClick}>
-                                            Iniciar Sesión
-                                        </Button>
-
-                                    </ModalFooter>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <div className="grid grid-cols-1 lg:grid-cols-2">
+                                    <div className='flex flex-col'>
+                                        <ModalHeader className="flex flex-col gap-1">Bienvenido</ModalHeader>
+                                        <ModalBody>
+                                            <Controller
+                                                name="username"
+                                                control={control}
+                                                rules={{ required: true }}
+                                                render={({ field }) => (
+                                                    <Input
+                                                        {...field}
+                                                        autoFocus
+                                                        label="Usuario"
+                                                        type="text"
+                                                        placeholder="Ingresa tu usuario"
+                                                        variant="underlined"
+                                                        startContent={<UserIcon className='w-5 h-5' />}
+                                                        isInvalid={errors.username ? true : false}
+                                                    />
+                                                )}
+                                            />
+                                            <Controller
+                                                name="password"
+                                                control={control}
+                                                rules={{ required: true }}
+                                                render={({ field }) => (
+                                                    <Input
+                                                        {...field}
+                                                        label="Contraseña"
+                                                        placeholder="Ingresa tu contraseña"
+                                                        type={isVisiblePassword ? "text" : "password"}
+                                                        variant="underlined"
+                                                        startContent={<LockClosedIcon className='w-5 h-5' />}
+                                                        errorMessage={errors?.password?.message}
+                                                        endContent={
+                                                            <Button className="focus:outline-none bg-white" type="button" isIconOnly onClick={toggleVisibility}>
+                                                                {isVisiblePassword ? (
+                                                                    <EyeIcon className="w-5 h-5" />
+                                                                ) : (
+                                                                    <EyeSlashIcon className="w-5 h-5" />
+                                                                )}
+                                                            </Button>
+                                                        }
+                                                        isInvalid={errors.password ? true : false}
+                                                    />
+                                                )}
+                                            />
+                                        </ModalBody>
+                                        <ModalFooter>
+                                            <Button
+                                                color="danger"
+                                                variant="light"
+                                                radius="sm"
+                                                size='md'
+                                                onPress={onClose}>
+                                                Cerrar
+                                            </Button>
+                                            <Button
+                                                color='primary'
+                                                radius="sm"
+                                                fullWidth
+                                                size='md'
+                                                type='submit'>
+                                                Iniciar Sesión
+                                            </Button>
+                                        </ModalFooter>
+                                    </div>
+                                    <div className="hidden md:inline">
+                                        <img
+                                            className="w-full"
+                                            src="../images/Login.webp"
+                                            alt="Login Image" />
+                                    </div>
                                 </div>
-
-                                <div className="hidden lg:inline">
-                                    <img
-                                        className="w-full"
-                                        src="../images/Login.webp"
-                                        alt="Login Image" />
-                                </div>
-                            </div>
+                            </form>
                         </>
                     )}
                 </ModalContent>
-            </Modal>
+            </Modal >
         </>
     );
 }
