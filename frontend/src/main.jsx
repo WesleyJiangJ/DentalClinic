ee9567eef09f032d35b6afa3f62804072c831fed
Wesley Jiang <wjiang917@gmail.com>
2024-09-11 17:43:52 -0600

    Wrapped App with UserProvider to provide user context throughout the app
    - Added `UserProvider` around `App` to make user context available across the entire application.
    - This change allows components within the app to access and update user-related data using the `useUser` hook.
    - Ensured that `UserProvider` is within `BrowserRouter` to maintain routing functionality.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from './contexts/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-      <BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
         <App />
-      </BrowserRouter>
-    </React.StrictMode>
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
