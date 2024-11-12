57a347178807a13810f006683adcccf42eb7a9df
Wesley Jiang <wjiang917@gmail.com>
2024-11-12 15:35:11 -0600

    Refactor Notes component to dynamically fetch content type and improve UX
    - Added `isLoading` state to manage loading feedback for async operations.
    - Modified `onSubmit` and `delete` functions to show loading state and handle success/error feedback with `sweetToast`.
    - Introduced `getContentType` function to dynamically fetch content type based on the `from` prop.
    - Replaced static content type assignment with `setValue` for a more dynamic, API-based approach.
    - Simplified and enhanced reset logic after successful delete or submit operations.

--- frontend/src/components/dashboard/Notes.jsx
+++ frontend/src/components/dashboard/Notes.jsx
@@ -4,35 +4,51 @@ import { useForm, Controller } from "react-hook-form"
 import { Button, Card, CardHeader, CardBody, Textarea, Input, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
 import { PlusIcon, ArrowPathIcon, TrashIcon } from "@heroicons/react/24/solid"
 import { sweetAlert, sweetToast } from "./Alerts";
-import { deleteNote, getNote, postNote } from "../../api/apiFunctions";
+import { deleteNote, getContentType, getNote, postNote } from "../../api/apiFunctions";
 
 export default function Notes({ backgroundColor, from, loadData, notes }) {
     const param = useParams();
     const [noteID, setNoteID] = React.useState('');
-    const { control, handleSubmit, formState: { errors }, reset } = useForm({
+    const [isLoading, setIsLoading] = React.useState(false);
+    const { control, handleSubmit, formState: { errors }, reset, setValue, getValues } = useForm({
         defaultValues: {
             name: '',
             content: '',
             object_id: parseInt(param.id),
-            content_type: (from === 'PT' ? 10 : from === 'PS' ? 12 : 15) // Content Type: Patient = 10, Personal = 12 & Odontogram Teeth = 15
+            content_type: null
         }
     });
 
+    React.useEffect(() => {
+        loadContentType();
+    }, [getValues('content_type')]);
+
+    const loadContentType = async () => {
+        await getContentType('api', (from === 'PT' ? 'patient' : from === 'PS' ? 'personal' : 'odontogram'))
+            .then((data) => {
+                setValue('content_type', data.data.content_type_id);
+            });
+    }
+
     const onSubmit = async (data) => {
-        sweetToast('success', 'Nota guardada');
+        setIsLoading(true);
         await postNote(data)
             .then(() => {
                 loadData();
                 reset();
+                sweetToast('success', 'Nota guardada');
+                setIsLoading(false);
             })
             .catch((error) => {
+                sweetToast('error', 'Ha ocurrido un error');
+                setIsLoading(false);
                 console.error('Error: ', error);
             })
     }
     return (
         <Card
             radius="sm"
-            className={`h-full ${backgroundColor}`}
+            className={`h-full overflow-scroll ${backgroundColor}`}
             fullWidth>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <CardHeader className="flex flex-col gap-2">
@@ -47,15 +63,21 @@ export default function Notes({ backgroundColor, from, loadData, notes }) {
                                         color="danger"
                                         radius="sm"
                                         size="sm"
+                                        isLoading={isLoading}
                                         onClick={async () => {
-                                            await sweetAlert('¿Desea eliminar la nota?', '', 'warning', 'success', 'Nota Eliminada');
+                                            await sweetAlert('¿Desea eliminar la nota?', '', 'warning', 'info', 'Espere un momento...');
+                                            setIsLoading(true);
                                             await deleteNote(noteID)
                                                 .then(() => {
+                                                    sweetToast('success', 'Nota Eliminada');
                                                     loadData();
-                                                    reset({ name: '', content: '', object_id: parseInt(param.id), content_type: (from === 'PT' ? 10 : from === 'PS' ? 12 : 15) });
+                                                    reset({ name: '', content: '', object_id: parseInt(param.id), content_type: null });
                                                     setNoteID('');
+                                                    setIsLoading(false);
                                                 })
                                                 .catch((error) => {
+                                                    sweetToast('error', 'Ha ocurrido un error');
+                                                    setIsLoading(false);
                                                     console.error('Error: ', error);
                                                 })
                                         }}>
@@ -68,7 +90,7 @@ export default function Notes({ backgroundColor, from, loadData, notes }) {
                                         radius="sm"
                                         size="sm"
                                         onClick={() => {
-                                            reset({ name: '', content: '', object_id: parseInt(param.id), content_type: (from === 'PT' ? 10 : from === 'PS' ? 12 : 15) });
+                                            reset({ name: '', content: '', object_id: parseInt(param.id), content_type: null });
                                             setNoteID('');
                                         }}>
                                         <ArrowPathIcon className="w-5 h-5" />
@@ -82,6 +104,7 @@ export default function Notes({ backgroundColor, from, loadData, notes }) {
                                     variant="light"
                                     radius="sm"
                                     size="sm"
+                                    isLoading={isLoading}
                                     type="submit">
                                     <PlusIcon className="w-5 h-5" />
                                 </Button>
